7352ea4a38e0f06fcfa53f4e1b751787e2c297f4
webdevsk <72684302+webdevsk@users.noreply.github.com>
2026-01-11 23:33:14 +0600

    v1.04: Pulled changes from extension 1.0.4

--- src/lib/get-own-language-filters.ts
+++ src/lib/get-own-language-filters.ts
@@ -1,4 +1,7 @@
 import type { KeywordsPerLanguageItem } from "@/data/keywords-per-language"
 
+// Since navigator.languages from this site often returns unexpected values, append English filters by default
+export const getGlobalFilters = (obj: KeywordsPerLanguageItem) =>
+	Array.isArray(obj.en) ? obj.en : [obj.en]
 export const getOwnLangFilters = (obj: KeywordsPerLanguageItem) =>
 	navigator.languages.flatMap(lang => obj[lang])
